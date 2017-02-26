f843e423ad901b2db67274844f5759bd216a8996
Juho Niemistö <juho.niemisto@iki.fi>
2017-02-26 22:00:11 +0200

    Strip unwanted characters.

--- quiz1.rb
+++ quiz1.rb
@@ -1,4 +1,9 @@
 class SolitaireCipherEncoder
   def encode(msg)
+    strip_characters(msg)
+  end
+
+  def strip_characters(msg)
+    msg.gsub(/[^A-Za-z]/, "")
   end
 end
--- quiz1_spec.rb
+++ quiz1_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe SolitaireCipherEncoder do
     encoded = SolitaireCipherEncoder.new.encode("Code in Ruby, live longer!")
     expect(encoded).to eq "DWJXH YRFDG TMSHP UURXJ"
   end
+
+  describe "character striping" do
+    it "keeps a-z characters" do
+      msg = "HelloRuby"
+      striped_str = SolitaireCipherEncoder.new.strip_characters(msg)
+      expect(striped_str).to eq msg
+    end
+    it "stripes non encoded characters" do
+      striped_str = SolitaireCipherEncoder.new.strip_characters(",-131!")
+      expect(striped_str).to eq ""
+    end
+  end
 end
