97114990a1135b422f67dd8bda38b39f7177acda
Juho Niemistö <juho.niemisto@iki.fi>
2017-02-27 16:14:02 +0200

    Fix wrong expected encoding result.

--- quiz1_spec.rb
+++ quiz1_spec.rb
@@ -3,7 +3,7 @@ require_relative "quiz1"
 RSpec.describe SolitaireCipherEncoder do
   it "encodes given example" do
     encoded = SolitaireCipherEncoder.new.encode("Code in Ruby, live longer!")
-    expect(encoded).to eq "DWJXH YRFDG TMSHP UURXJ"
+    expect(encoded).to eq "GLNCQ MJAFF FVOMB JIYCB"
   end
 
   describe "character striping" do
